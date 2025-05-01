07841a3eb8893bf035c2d0099428bfd75cc2a3c3
Vivek241197 <kvivak952@gmail.com>
2025-05-01 19:51:56 +0530

    Change in branch 1

---
+++ ServiceRegistry/src/main/java/com/example/serviceRegistry/ServiceRegistry/Controll.java
@@ -0,0 +1,12 @@
+package com.example.serviceRegistry.ServiceRegistry;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Controll {
+	
+	public void setSomeId() {
+		System.out.println("Doing something");
+	}
+
+}
